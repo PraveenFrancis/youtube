e2a03da9886504e488bed7434902269d066b1ab8
PraveenFrancis <praveenfrancis555@gmail.com>
2025-01-03 15:19:24 +0530

    ui changes

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Button from './Button'
 import VideoContainer from './VideoContainer'
+import { useSelector } from "react-redux";
 
 const MainContainer = () => {
+  const isOpenHambergerMenu = useSelector((store) => store.app.isMenuOpen);
+
   return (
-    <div className='flex flex-col ml-44 mt-16 z-0'>
+    <div  className={`${
+      !isOpenHambergerMenu ? "ml-0" : "ml-44"
+    } flex flex-col ml-44 mt-16 z-0`}>
         <Button></Button>
         <VideoContainer></VideoContainer>
     </div>
